d55f6f4e0bbaae0cca3236f2411ea81953b74e22
Gaurav Kadam <gauravkadam2422@gmail.com>
2026-05-12 16:08:56 +0530

    feat(database): setup PostgreSQL with Docker and EF Core

---
+++ server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
